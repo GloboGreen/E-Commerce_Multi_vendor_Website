90ab127eff711b0487e1d1090df6f482cdfa32f5
M Subash <105859085+Git-Subash@users.noreply.github.com>
2025-03-11 16:48:32 +0530

    updated/fronted

--- Client/src/lib/lookUpMap.ts
+++ Client/src/lib/lookUpMap.ts
@@ -6,16 +6,16 @@ export function createLookup<T>(
   return new Map(array.map((item) => [String(item[key]), String(item[value])]));
 }
 
-function userLookup<T, K extends keyof T>(
-  array: T[],
-  key: K,
-  value: keyof T,
-): Map<T[K], T[typeof value]> {
-  const map = new Map<T[K], T[typeof value]>();
-  array.forEach((item) => {
-    if (item[key] !== undefined && item[value] !== undefined) {
-      map.set(item[key], item[value]);
-    }
-  });
-  return map;
-}
+// function userLookup<T, K extends keyof T>(
+//   array: T[],
+//   key: K,
+//   value: keyof T,
+// ): Map<T[K], T[typeof value]> {
+//   const map = new Map<T[K], T[typeof value]>();
+//   array.forEach((item) => {
+//     if (item[key] !== undefined && item[value] !== undefined) {
+//       map.set(item[key], item[value]);
+//     }
+//   });
+//   return map;
+// }
